62c44786d6eb7e08640d8dc25988a5251b09f35a
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-17 05:00:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ def now_serving(other_deli)
   if other_deli.length == 0
   puts "There is nobody waiting to be served!"
 else
-  puts "Currently serving #{other_deli[1]}."
+  puts "Currently serving #{other_deli[0]}."
 
 end
 
